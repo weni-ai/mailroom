964c9199692673ce8516b11225b75f3955909095
Paulo Bernardo <paulo.bernardo@weni.ai>
2024-04-18 19:11:36 -0300

    feat: use queue type as metric label

--- runtime/metrics/metrics.go
+++ runtime/metrics/metrics.go
@@ -1,76 +1,30 @@
 package metrics
 
 import (
-	"github.com/nyaruka/mailroom/core/queue"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 func SetAvailableWorkers(workerQueue string, count int) {
-	switch workerQueue {
-	case queue.HandlerQueue:
-		HandlerWorkersAvailable.Set(float64(count))
-	case queue.BatchQueue:
-		BatchWorkersAvailable.Set(float64(count))
-	case queue.FlowBatchQueue:
-		FlowBatchWorkersAvailable.Set(float64(count))
-	}
+	AvailableWorkers.WithLabelValues(workerQueue).Set(float64(count))
 }
 
 func AddWorker(workerQueue string) {
-	switch workerQueue {
-	case queue.HandlerQueue:
-		HandlerWorkersInUse.Inc()
-		HandlerWorkersAvailable.Dec()
-	case queue.BatchQueue:
-		BatchWorkersInUse.Inc()
-		BatchWorkersAvailable.Dec()
-	case queue.FlowBatchQueue:
-		FlowBatchWorkersInUse.Inc()
-		FlowBatchWorkersAvailable.Dec()
-	}
+	UsedWorkers.WithLabelValues(workerQueue).Inc()
+	AvailableWorkers.WithLabelValues(workerQueue).Dec()
 }
 
 func RemoveWorker(workerQueue string) {
-	switch workerQueue {
-	case queue.HandlerQueue:
-		HandlerWorkersInUse.Dec()
-		HandlerWorkersAvailable.Inc()
-	case queue.BatchQueue:
-		BatchWorkersInUse.Dec()
-		BatchWorkersAvailable.Inc()
-	case queue.FlowBatchQueue:
-		FlowBatchWorkersInUse.Dec()
-		FlowBatchWorkersAvailable.Inc()
-	}
+	UsedWorkers.WithLabelValues(workerQueue).Dec()
+	AvailableWorkers.WithLabelValues(workerQueue).Inc()
 }
 
-var HandlerWorkersInUse = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "mr_handler_workers_in_use",
-	Help: "The number of handler workers currently in use",
-})
+var UsedWorkers = promauto.NewGaugeVec(prometheus.GaugeOpts{
+	Name: "mr_used_workers",
+	Help: "The number of workers currently in use",
+}, []string{"queue"})
 
-var HandlerWorkersAvailable = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "mr_handler_workers_available",
-	Help: "The number of handler workers currently available",
-})
-
-var BatchWorkersInUse = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "mr_batch_workers_in_use",
-	Help: "The number of batch workers currently in use",
-})
-
-var BatchWorkersAvailable = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "mr_batch_workers_available",
-	Help: "The number of batch workers currently available",
-})
-
-var FlowBatchWorkersInUse = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "mr_flow_batch_workers_in_use",
-	Help: "The number of flow batch workers currently in use",
-})
-
-var FlowBatchWorkersAvailable = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "mr_flow_batch_workers_available",
-	Help: "The number of flow batch workers currently available",
-})
+var AvailableWorkers = promauto.NewGaugeVec(prometheus.GaugeOpts{
+	Name: "mr_available_workers",
+	Help: "The number of workers currently available",
+}, []string{"queue"})
