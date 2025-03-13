a709d9cc8a85bf9ea15ad8a3d2798ef65f24ee31
Robi9 <robertasousa714@gmail.com>
2025-03-13 14:35:05 -0300

    Fix locale field

--- core/models/msgs.go
+++ core/models/msgs.go
@@ -1464,7 +1464,7 @@ type WppBroadcastTemplate struct {
 	UUID      assets.TemplateUUID `json:"uuid" validate:"required,uuid"`
 	Name      string              `json:"name" validate:"required"`
 	Variables []string            `json:"variables,omitempty"`
-	Locale    string              `json:"locale" validate:"required"` // ex: "pt-BR"
+	Locale    string              `json:"locale,omitempty"`
 }
 
 type WppBroadcastMessageHeader struct {
